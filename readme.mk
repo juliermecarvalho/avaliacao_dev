015f7288c44211f6ef1d9d669dddfbd15a4b43e3
julierme <juliermecarvalho@gmail.com>
2023-02-14 16:30:33 -0300

    remomear pastas

--- readme.mk
+++ readme.mk
@@ -2,18 +2,31 @@
 Avaliação Dev para B3
 
 
-Abrir a o .sln na pasta "B3.Api"
+Instruções para Abrir o Projeto B3.Api no Visual Studio:
 
-1. abrir o projeto no Vistual Studio
-2. definir como projeto de inicialização  "B3.Api" play F5
-3. Projeto B3.Test é projeto Visual studio não foi usado nenhuma ferramenta de terceiros.
-4. rodar todos os testes (ctrl+r, v)
+1. Abra o Visual Studio e selecione "Abrir um Projeto ou Solução".
+2. Navegue até a pasta "B3.Api" e selecione o arquivo com a extensão ".sln".
+3. Quando o projeto for aberto, clique com o botão direito do mouse no projeto "B3.Api" na janela "Solução" e selecione "Definir como Projeto de Inicialização".
+4 Pressione "F5" para iniciar o projeto ou clique no ícone de play na barra de ferramentas.
 
-Abrir a  pasta "B3.Site\b3site" no "vscode"
-1. npm install
-2. ng server --o
-3. verifica no arquivo "services/investment.service.ts"
-4. a variavel "private readonly API = 'http://localhost:32826/';" está correta a projeto do visual statudio
+
+Instruções para Executar os Testes no Projeto B3.Api:
+
+1. No Visual Studio, clique na guia "Teste" na barra de menu.
+2. Selecione "Executar todos os testes" ou pressione "Ctrl + R, V".
+3. Verifique se todos os testes foram executados com sucesso.
+
+
+Instruções para Abrir o Projeto B3.Site no Visual Studio Code:
+
+1. Abra o Visual Studio Code e selecione "Abrir pasta".
+2. Navegue até a pasta "B3.Site\b3site" e selecione-a.
+3 .Abra o terminal (Ctrl + ` ou clicando no ícone de terminal na barra de ferramentas) e execute o comando "npm install".
+4. Após a instalação das dependências, execute o comando "ng serve --o".
+5 Verifique se a variável "private readonly API = 'http://localhost:32826/';" no arquivo "services/investment.service.ts" está configurada corretamente.
+
+
+Com essas instruções, você deve ser capaz de abrir e executar ambos os projetos com sucesso.
 
 
 
