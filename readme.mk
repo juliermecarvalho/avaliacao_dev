9280349bd96b7b9574b2694177ab3c57283d734f
julierme <juliermecarvalho@gmail.com>
2023-02-14 16:25:53 -0300

    remomear pastas

--- readme.mk
+++ readme.mk
@@ -1,2 +1,19 @@
 # avaliacao_dev
 Avaliação Dev para B3
+
+
+Abrir a o .sln na pasta "B3.Api"
+
+1. abrir o projeto no Vistual Studio
+2. definir como projeto de inicialização  "B3.Api" play F5
+3. Projeto B3.Test é projeto Visual studio não foi usado nenhuma ferramenta de terceiros.
+4. rodar todos os testes (ctrl+r, v)
+
+Abrir a  pasta "B3.Site\b3site" no "vscode"
+1. npm install
+2. ng server --o
+3. verifica no arquivo "services/investment.service.ts"
+4. a variavel "private readonly API = 'http://localhost:32826/';" está correta a projeto do visual statudio
+
+
+
