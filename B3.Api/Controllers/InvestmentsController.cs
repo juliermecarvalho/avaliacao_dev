1d6738b788a4df185d32f2c143158b19005c8f5e
JuliermeCarvalho <JuliermeCarvalho@NB1049>
2023-02-14 10:32:56 -0300

    Add project files.

---
+++ B3.Api/Controllers/InvestmentsController.cs
@@ -0,0 +1,27 @@
+﻿using B3.Api.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace B3.Api.Controllers
+{
+    public class InvestmentsController : ApiController
+    {
+
+        [HttpPost]
+        public decimal CalculateFinalValue(Investment investment)
+        {
+            decimal cdi = 0.009m;
+            decimal tb = 1.08m;
+         
+
+            return investment.CalculateFinalValue(cdi, tb);
+        }
+
+       
+
+    }
+}
