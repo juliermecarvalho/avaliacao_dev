ea9448fa42fa38d3e2e7ec021e25bea6c8cf0abc
Julierme Carvalho <julierme@trustimage.com.br>
2024-11-14 07:37:44 -0300

    ajuste no script para também rodar os testes

--- start-b3-environment.ps1
+++ start-b3-environment.ps1
@@ -1,11 +1,12 @@
 # Define os caminhos das pastas
-$apiPath = "B3\B3.Api"
+$apiTest = "B3\B3.Test"
+$apiPath = "..\B3\B3.Api"
 $sitePath = "B3\B3.Site\b3site"
 
-# Abrir a API .NET
-Write-Host "Abrindo a API em: $apiPath"
-Start-Process powershell -ArgumentList "-NoExit", "-Command", "cd $apiPath; dotnet run"
+# Executar os testes e iniciar a API .NET em uma única janela
+Write-Host "Executando os testes e abrindo a API em: $apiTest e $apiPath"
+Start-Process powershell -ArgumentList "-NoExit", "-Command", "cd $apiTest; dotnet restore; dotnet test --verbosity minimal; cd ..\$apiPath; dotnet restore; dotnet run"
 
-# Abrir o site Angular
+# Abrir o site Angular em uma nova janela
 Write-Host "Abrindo o site Angular em: $sitePath"
 Start-Process powershell -ArgumentList "-NoExit", "-Command", "cd $sitePath; npm install --force or --legacy-peer-deps; ng serve -o"
